cf5fcd3c21c8517b20b9c9a467c06e47d2c96209
Jonathan Wedin <jonathan.wedin@vembla.se>
2021-06-04 16:36:33 +0200

    Refactor: List cleaned up.

--- containers/List/index.jsx
+++ containers/List/index.jsx
@@ -1,54 +1,20 @@
 import React from "react";
 import { useReducer } from "react";
 import { useState } from "react";
-import {
-  View,
-  TextInput,
-  Text,
-  SafeAreaView,
-  Button,
-  TouchableOpacity,
-} from "react-native";
+import { View, TextInput, Text, SafeAreaView, Button } from "react-native";
+import { itemReducer } from "../../reducers/item";
+import { ACTIONS } from "../../utils/constants/actions";
+import { ProductItem } from "../../components/ProductItem";
 
-const ACTIONS = {
-  ADD_ITEM: "ADD_ITEM",
-  INCREASE_QUANTITY: "INCREASE_QUANTITY",
-  DECREASE_QUANTITY: "DECREASE_QUANTITY",
+const initialState = {
+  "93bfc1cb-0698-4acd-8cee-52506e737f48": { name: "Milk", quantity: 1 },
 };
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case ACTIONS.ADD_ITEM:
-      return {
-        ...state,
-        [Object.values(state).length]: { name: action.payload, quantity: 1 },
-      };
-
-    case ACTIONS.DECREASE_QUANTITY:
-      return {
-        ...state,
-        [action.payload]: {
-          ...state[action.payload],
-          quantity: state[action.payload].quantity - 1,
-        },
-      };
-
-    case ACTIONS.INCREASE_QUANTITY:
-      return {
-        ...state,
-        [action.payload]: {
-          ...state[action.payload],
-          quantity: state[action.payload].quantity + 1,
-        },
-      };
-  }
-};
-
-const initialState = { 0: { name: "Milk", quantity: 1 } };
+// Låtsas att det finns en server. Jobba med mockade API:er.
 
 export const List = () => {
   const [input, setInput] = useState("");
-  const [items, dispatch] = useReducer(reducer, initialState);
+  const [items, dispatch] = useReducer(itemReducer, initialState);
 
   const handlePress = () => {
     dispatch({ type: ACTIONS.ADD_ITEM, payload: input });
@@ -63,14 +29,19 @@ export const List = () => {
     dispatch({ type: ACTIONS.INCREASE_QUANTITY, payload: id });
   };
 
+  const handleDelete = (id) => {
+    dispatch({ type: ACTIONS.DELETE_ITEM, payload: id });
+  };
+
   return (
-    <View
+    <SafeAreaView
       style={{
         flex: 1,
       }}
     >
       <TextInput
         onChangeText={setInput}
+        onSubmitEditing={handlePress}
         value={input}
         style={{ backgroundColor: "#FFF", height: 40, width: "100%" }}
       />
@@ -83,63 +54,18 @@ export const List = () => {
           paddingTop: 30,
         }}
       >
-        {Object.entries(items).map((item) => (
-          <View
-            key={item[0]}
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between",
-              width: "50%",
-              marginBottom: 20,
-            }}
-          >
-            <Text style={{ fontSize: 20 }} key={item[0]}>
-              {item[1].name}
-            </Text>
-            <View
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-              }}
-            >
-              <QuantityButton
-                text="-"
-                onPress={() => handleDecrease(item[0])}
-              />
-              <Text style={{ fontSize: 20 }}>{item[1].quantity}</Text>
-              <QuantityButton
-                text="+"
-                onPress={() => handleIncrease(item[0])}
-              />
-            </View>
-          </View>
+        {/* show something if empty list */}
+        {Object.entries(items).map(([id, item]) => (
+          <ProductItem
+            key={id}
+            id={id}
+            item={item}
+            handleDecrease={handleDecrease}
+            handleIncrease={handleIncrease}
+            handleDelete={handleDelete}
+          />
         ))}
       </View>
-    </View>
-  );
-};
-
-export const QuantityButton = ({ text, onPress }) => {
-  return (
-    <TouchableOpacity
-      onPress={onPress}
-      style={{
-        borderRadius: 15,
-        backgroundColor: "grey",
-        width: 30,
-        height: 30,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text
-        style={{ color: "white", textAlignVertical: "center", fontSize: 20 }}
-      >
-        {text}
-      </Text>
-    </TouchableOpacity>
+    </SafeAreaView>
   );
 };
